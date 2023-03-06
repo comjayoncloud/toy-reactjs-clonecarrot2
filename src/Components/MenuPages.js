0a4bd4812a839d168548eb57dc31e84469145e4b
jaewoo <comjayoncloud@gmail.com>
2023-03-06 19:40:49 +0900

    update

--- src/Components/MenuPages.js
+++ src/Components/MenuPages.js
@@ -3,11 +3,15 @@ import "../Scss/MenuPages.scss";
 export default function MenuPages() {
   return (
     <div className="MenuPages">
-      <div>중고거래</div>
-      <div>동네가게</div>
-      <div>알바</div>
-      <div>부동산직거래</div>
-      <div>중고차직거래</div>
+      <nav>
+        <ul>
+          <li>중고거래</li>
+          <li>동네가게</li>
+          <li>알바</li>
+          <li>부동산직거래</li>
+          <li>중고차직거래</li>
+        </ul>
+      </nav>
     </div>
   );
 }
